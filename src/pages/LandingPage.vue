79617d7f5114b4e7bafd9901ff8e961160973512
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-19 20:56:00 +0800

    update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,59 +1,93 @@
 <template>
+  <header></header>
   <body>
-    <div class="container">
-      <h5 class="center">Waktu Solat Malaysia</h5>
+    <main>
+      <div class="container">
+        <div
+          class="card-panel"
+          style="background-color: rgba(255, 255, 255, 0.6)"
+        >
+          <h5 class="center">Waktu Solat Malaysia</h5>
 
-      <div v-if="!isLoading" class="row">
-        <div class="col s12 m4 l4 push-m2 push-l2">
-          <vue3-simple-typeahead
-            id="typeahead_id2"
-            placeholder="1. Taip Negeri"
-            :items="this.negeri"
-            :minInputLength="1"
-            @selectItem="selectNegeri"
-          >
-          </vue3-simple-typeahead>
-          <label for="typeahead_id2">1️⃣Taip Negeri</label>
-        </div>
-        <div class="col s12 m4 m4 push-m2 push-l2">
-          <vue3-simple-typeahead
-            id="typeahead_id"
-            placeholder="2. Taip Bandar/Daerah"
-            :items="this.senaraiDaerah"
-            :minInputLength="1"
-            @selectItem="selectDaerah"
-          >
-          </vue3-simple-typeahead>
-          <label for="typeahead_id">2️⃣Taip Bandar/Daerah</label>
+          <div v-if="!isLoading" class="row">
+            <div class="col s12 m4 l4 push-m2 push-l2">
+              <vue3-simple-typeahead
+                id="typeahead_id2"
+                placeholder="1. Taip Negeri"
+                :items="this.negeri"
+                :minInputLength="1"
+                @selectItem="selectNegeri"
+              >
+              </vue3-simple-typeahead>
+              <label for="typeahead_id2">1️⃣Taip Negeri</label>
+            </div>
+            <div class="col s12 m4 m4 push-m2 push-l2">
+              <vue3-simple-typeahead
+                id="typeahead_id"
+                placeholder="2. Taip Bandar/Daerah"
+                :items="this.senaraiDaerah"
+                :minInputLength="1"
+                @selectItem="selectDaerah"
+              >
+              </vue3-simple-typeahead>
+              <label for="typeahead_id">2️⃣Taip Bandar/Daerah</label>
+            </div>
+          </div>
+
+          <div v-if="isLoading" class="center">Data Loading . . .⏳</div>
+
+          <div v-if="!isLoading" class="row">
+            <div class="col s12 m8 l8 push-m2 push-l2">
+              <table class="responsive-table centered">
+                <thead>
+                  <tr>
+                    <th>Imsak</th>
+                    <th>Subuh</th>
+                    <th>Syuruk</th>
+                    <th>Zohor</th>
+                    <th>Asar</th>
+                    <th>Maghrib</th>
+                    <th>Isyak</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td v-for="time in waktuSolat" :key="time">{{ time }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div v-if="isLoading" class="center">Data Loading . . .⏳</div>
-
-      <div v-if="!isLoading" class="row">
-        <div class="col s12 m8 l8 push-m2 push-l2">
-          <table class="responsive-table centered">
-            <thead>
-              <tr>
-                <th>Imsak</th>
-                <th>Subuh</th>
-                <th>Syuruk</th>
-                <th>Zohor</th>
-                <th>Asar</th>
-                <th>Maghrib</th>
-                <th>Isyak</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td v-for="time in waktuSolat" :key="time">{{ time }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    </main>
+  </body>
+  <footer class="page-footer" style="background-color: transparent">
+    <div class="footer-copyright" style="background-color: #00796b">
+      <div class="container">
+        © 2022 Copyright Text
+        <a
+          class="grey-text text-lighten-4 left"
+          href="https://zaimramlan.github.io/waktu-solat-api/"
+          ><u>Prayer Time API</u></a
+        >
+        <span class="right">
+          Photo by
+          <a
+            href="https://unsplash.com/@fahrulazmi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            class="grey-text text-lighten-4"
+            ><u>Fahrul Azmi</u></a
+          >
+          on
+          <a
+            href="https://unsplash.com/s/photos/mosque-malaysia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            class="grey-text text-lighten-4"
+            ><u>Unsplash</u></a
+          >
+        </span>
       </div>
     </div>
-  </body>
+  </footer>
 </template>
 
 <script>
@@ -128,4 +162,19 @@ export default {
 </script>
 
 <style>
+body {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+
+  background-image: url("../assets/bg.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+}
+
+main {
+  flex: 1 0 auto;
+}
 </style>
