1816e7bcea0a115cb7373ea2d7ef4b5c913e3482
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-20 00:31:20 +0800

    setup local storage

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -3,59 +3,79 @@
   <body>
     <main>
       <div class="container">
-        <div
-          class="card-panel"
-          style="background-color: rgba(255, 255, 255, 0.6); margin-top: 50px"
-        >
-          <h5 class="center">Waktu Solat Malaysia</h5>
+        <div class="row">
+          <div class="col s12 m10 l10 push-m1 push-l1">
+            <div
+              class="card-panel"
+              style="
+                background-color: rgba(255, 255, 255, 0.6);
+                margin-top: 50px;
+              "
+            >
+              <h5 class="center capital-first">Waktu Solat Malaysia</h5>
 
-          <div v-if="!isLoading" class="row">
-            <div class="col s12 m4 l4 push-m2 push-l2">
-              <vue3-simple-typeahead
-                id="typeahead_id2"
-                placeholder="1. Taip Negeri"
-                :items="this.negeri"
-                :minInputLength="1"
-                @selectItem="selectNegeri"
-              >
-              </vue3-simple-typeahead>
-              <label for="typeahead_id2">1️⃣Taip Negeri</label>
-            </div>
-            <div class="col s12 m4 m4 push-m2 push-l2">
-              <vue3-simple-typeahead
-                id="typeahead_id"
-                placeholder="2. Taip Bandar/Daerah"
-                :items="this.senaraiDaerah"
-                :minInputLength="1"
-                @selectItem="selectDaerah"
-              >
-              </vue3-simple-typeahead>
-              <label for="typeahead_id">2️⃣Taip Bandar/Daerah</label>
-            </div>
-          </div>
+              <div v-if="!isLoading" class="row">
+                <div class="col s12 m4 l4 push-m2 push-l2">
+                  <vue3-simple-typeahead
+                    id="typeahead_id2"
+                    placeholder="Negeri"
+                    :items="this.negeri"
+                    :minInputLength="1"
+                    @selectItem="selectNegeri"
+                  >
+                  </vue3-simple-typeahead>
+                  <label style="font-size: large" for="typeahead_id2"
+                    >1. Taip Negeri⬆️</label
+                  >
+                </div>
+                <div class="col s12 m4 l4 push-m2 push-l2">
+                  <vue3-simple-typeahead
+                    id="typeahead_id"
+                    placeholder="Bandar/Daerah"
+                    :items="this.senaraiDaerah"
+                    :minInputLength="1"
+                    @selectItem="selectDaerah"
+                  >
+                  </vue3-simple-typeahead>
+                  <label style="font-size: large" for="typeahead_id"
+                    >2. Taip Bandar/Daerah⬆️</label
+                  >
+                </div>
+              </div>
 
-          <div v-if="isLoading" class="center">Data Loading . . .⏳</div>
+              <div v-if="isLoading" class="center">Data Loading . . .⏳</div>
 
-          <div v-if="!isLoading" class="row">
-            <div class="col s12 m8 l8 push-m2 push-l2">
-              <table class="responsive-table centered">
-                <thead>
-                  <tr>
-                    <th>Imsak</th>
-                    <th>Subuh</th>
-                    <th>Syuruk</th>
-                    <th>Zohor</th>
-                    <th>Asar</th>
-                    <th>Maghrib</th>
-                    <th>Isyak</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td v-for="time in waktuSolat" :key="time">{{ time }}</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div v-if="!isLoading">
+                <div class="center" style="font-weight: bold">
+                  {{ capitalizeFirstLetter(negeriSave) }},
+                  {{ capitalizeFirstLetter(daerahSave) }}
+                  <br />{{ dateSave }}
+                </div>
+                <table class="responsive-table centered">
+                  <thead>
+                    <tr>
+                      <th><u>Imsak</u></th>
+                      <th><u>Subuh</u></th>
+                      <th><u>Syuruk</u></th>
+                      <th><u>Zohor</u></th>
+                      <th><u>Asar</u></th>
+                      <th><u>Maghrib</u></th>
+                      <th><u>Isyak</u></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td
+                        style="font-weight: bold"
+                        v-for="time in waktuSolat"
+                        :key="time"
+                      >
+                        {{ time24to12(time) }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
@@ -115,29 +135,75 @@ export default {
         "johor",
       ],
       senaraiDaerah: [],
-
+      dateSave: "",
+      negeriSave: "",
+      daerahSave: "",
       isLoading: false,
-
-      temp: "",
-      temp2: "",
     };
   },
   mounted() {
     document.title = "Waktu Solat Malaysia";
     window.M.AutoInit();
+
     this.isLoading = true;
     return axios
       .get("https://waktu-solat-api.herokuapp.com/api/v1/prayer_times.json")
       .then((response) => (this.stateList = response.data.data.negeri))
       .then(() => {
         this.isLoading = false;
+      })
+      .then(() => {
+        try {
+          const localStateIndex = localStorage.getItem("stateIndex");
+          const localCityIndex = localStorage.getItem("cityIndex");
+          this.dateSave = localStorage.getItem("dateSave");
+          this.negeriSave = localStorage.getItem("negeriSave");
+          this.daerahSave = localStorage.getItem("daerahSave");
+
+          for (let i = 0; i < 7; i++) {
+            this.waktuSolat[i] =
+              this.stateList[localStateIndex].zon[localCityIndex].waktu_solat[
+                i
+              ].time;
+          }
+        } catch {
+          return 0;
+        }
       });
   },
   methods: {
+    selectNegeri(a) {
+      const nowDate = new Date();
+      this.dateSave =
+        nowDate.getDate() +
+        "/" +
+        (nowDate.getMonth() + 1) +
+        "/" +
+        nowDate.getFullYear();
+      localStorage.setItem("dateSave", this.dateSave);
+
+      localStorage.setItem("negeriSave", a);
+
+      const result = this.stateList.findIndex(({ nama }) => nama === a);
+      this.stateIndex = result;
+
+      localStorage.setItem("stateIndex", result);
+
+      const numberOfDaerah = this.stateList[this.stateIndex].zon.length;
+      for (let i = 0; i < numberOfDaerah; i++) {
+        this.senaraiDaerah[i] = this.stateList[this.stateIndex].zon[i].nama;
+      }
+    },
     selectDaerah(a) {
+      this.negeriSave = localStorage.getItem("negeriSave");
+      this.daerahSave = a;
+      localStorage.setItem("daerahSave", a);
+
       const cityIndex = this.stateList[this.stateIndex].zon.findIndex(
         ({ nama }) => nama === a
       );
+
+      localStorage.setItem("cityIndex", cityIndex);
 
       this.waktuLength =
         this.stateList[this.stateIndex].zon[cityIndex].waktu_solat.length;
@@ -147,15 +213,22 @@ export default {
           this.stateList[this.stateIndex].zon[cityIndex].waktu_solat[i].time;
       }
     },
-    selectNegeri(a) {
-      const result = this.stateList.findIndex(({ nama }) => nama === a);
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    time24to12(time) {
+      // Check correct time format and split into components
+      time = time
+        .toString()
+        .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
-      this.stateIndex = result;
-
-      const numberOfDaerah = this.stateList[this.stateIndex].zon.length;
-      for (let i = 0; i < numberOfDaerah; i++) {
-        this.senaraiDaerah[i] = this.stateList[this.stateIndex].zon[i].nama;
+      if (time.length > 1) {
+        // If time format correct
+        time = time.slice(1); // Remove full string match value
+        time[5] = +time[0] < 12 ? " AM" : " PM"; // Set AM/PM
+        time[0] = +time[0] % 12 || 12; // Adjust hours
       }
+      return time.join(""); // return adjusted time or original string
     },
   },
 };
